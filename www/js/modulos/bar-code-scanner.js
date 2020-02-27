284efd5c379d7277a690e3b5ca2a5acb2cd4097d
edsondepaulla <edsondepaulla@gmail.com>
2020-02-27 11:18:26 -0300

    teste

--- www/js/modulos/bar-code-scanner.js
+++ www/js/modulos/bar-code-scanner.js
@@ -1,4 +1,23 @@
 var BarCodeScanner = {
+    getQrcode(text) {
+        Factory.ajax(
+            {
+                action: 'qrcode/get',
+                data: {
+                    TEXT: text
+                }
+            },
+            function (data) {
+                if (data.status == 1) {
+                    if (parseInt(data.TRANSACAO_ID))
+                        Factory.$rootScope.transacaoId = parseInt(data.TRANSACAO_ID);
+
+                    if (data.url)
+                        Factory.$rootScope.location(data.url);
+                }
+            }
+        );
+    },
     qrcode: function () {
         try {
             if (Factory.$rootScope.device == 'android')
@@ -6,25 +25,8 @@ var BarCodeScanner = {
 
             cordova.plugins.barcodeScanner.scan(
                 function (result) {
-                    if (result.text) {
-                        Factory.ajax(
-                            {
-                                action: 'qrcode/get',
-                                data: {
-                                    TEXT: result.text
-                                }
-                            },
-                            function (data) {
-                                if (data.status == 1) {
-                                    if (parseInt(data.TRANSACAO_ID))
-                                        Factory.$rootScope.transacaoId = parseInt(data.TRANSACAO_ID);
-
-                                    if (data.url)
-                                        Factory.$rootScope.location(data.url);
-                                }
-                            }
-                        );
-                    }
+                    if (result.text)
+                        BarCodeScanner.getQrcode(result.text);
                 },
                 function (error) {
 
@@ -43,6 +45,26 @@ var BarCodeScanner = {
                 }
             );
         } catch (err) {
+            try {
+                navigator.notification.prompt(
+                    'Digite o código',
+                    function (results) {
+                        if (results.buttonIndex == 1) {
+                            if (results.input1.length)
+                                BarCodeScanner.getQrcode(results.input1);
+                            else
+                                return false;
+                        }
+                    },
+                    'Atenção',
+                    ['Continue', 'Cancelar'],
+                    ''
+                );
+            } catch (err) {
+                var text = prompt("Digite o código", "");
+                if (text != null)
+                    BarCodeScanner.getQrcode(text);
+            }
         }
     }
 };
