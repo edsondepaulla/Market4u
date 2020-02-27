51bd1f6cd1737fdb99b396ace77aa7d7e24cb284
edsondepaulla <edsondepaulla@gmail.com>
2020-02-27 09:55:22 -0300

    teste

--- www/js/modulos/qr-code.js
+++ www/js/modulos/qr-code.js
@@ -37,17 +37,17 @@ var QRScannerConf = {
                     alert("Scanning failed: " + error);
                 },
                 {
-                    preferFrontCamera : true, // iOS and Android
+                    preferFrontCamera : false, // iOS and Android
                     showFlipCameraButton : true, // iOS and Android
                     showTorchButton : true, // iOS and Android
-                    torchOn: true, // Android, launch with the torch switched on (if available)
-                    saveHistory: true, // Android, save scan history (default false)
-                    prompt : "Place a barcode inside the scan area", // Android
+                    torchOn: false, // Android, launch with the torch switched on (if available)
+                    saveHistory: false, // Android, save scan history (default false)
+                    prompt : "", // Android
                     resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
                    // formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-                    orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+                    orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
                     disableAnimations : true, // iOS
-                    disableSuccessBeep: false // iOS and Android
+                    disableSuccessBeep: true // iOS and Android
                 }
             );
 
