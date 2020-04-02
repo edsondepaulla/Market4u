ecee0b83ba8f99ac81bcb47a44e03d5df5033988
edsondepaulla <edsondepaulla@gmail.com>
2020-04-02 12:42:42 -0300

    x

--- www/js/modulos/bluetooth.js
+++ www/js/modulos/bluetooth.js
@@ -6,54 +6,61 @@ var bluetooth = {
         try {
             ble.enable(
                 function () {
-                    Factory.$rootScope.location('#!/command/18+/destravar/BLUETOOTH', 0, 1);
-                    ble.scan(
-                        [],
-                        5,
-                        function (device) {
-                            if (device.name == 'market4u') {
-                                bluetooth.deviceId = device.id;
-                                try {
-                                    ble.stopScan(
-                                        function () {
-                                        },
-                                        function () {
-                                        }
-                                    );
-                                } catch (e) {
-                                }
-                                ble.connect(
-                                    bluetooth.deviceId,
-                                    function (peripheral) {
-                                        var characteristic = peripheral.characteristics.filter(function (element) {
-                                            if (element.characteristic.toLowerCase() === 'ffe1')
-                                                return element;
-                                        })[0];
-                                        bluetooth.writeWithoutResponse = characteristic.properties.indexOf('WriteWithoutResponse') > -1 ? true : false;
-                                        ble.startNotification(
-                                            bluetooth.deviceId,
-                                            'ffe0',
-                                            'ffe1',
-                                            function (data) {
-
-                                            },
-                                            bluetooth.disconnect
-                                        );
-                                        bluetooth.sendData('1');
-                                    },
-                                    bluetooth.disconnect
-                                );
-                            }
-                        },
-                        bluetooth.disconnect
-                    );
+                    bluetooth.scan();
                 },
                 function () {
                     Factory.alert("LIGAR BLUETOOTH");
                 }
             );
         } catch (e) {
+            bluetooth.scan();
+        }
+    },
+    scan: function () {
+        try {
+            ble.scan(
+                [],
+                5,
+                function (device) {
+                    if (device.name == 'market4u') {
+                        Factory.$rootScope.location('#!/command/18+/destravar/BLUETOOTH', 0, 1);
+                        bluetooth.deviceId = device.id;
+                        try {
+                            ble.stopScan(
+                                function () {
+                                },
+                                function () {
+                                }
+                            );
+                        } catch (e) {
+                        }
+                        ble.connect(
+                            bluetooth.deviceId,
+                            function (peripheral) {
+                                var characteristic = peripheral.characteristics.filter(function (element) {
+                                    if (element.characteristic.toLowerCase() === 'ffe1')
+                                        return element;
+                                })[0];
+                                bluetooth.writeWithoutResponse = characteristic.properties.indexOf('WriteWithoutResponse') > -1 ? true : false;
+                                ble.startNotification(
+                                    bluetooth.deviceId,
+                                    'ffe0',
+                                    'ffe1',
+                                    function (data) {
 
+                                    },
+                                    bluetooth.disconnect
+                                );
+                                bluetooth.sendData('1');
+                            },
+                            bluetooth.disconnect
+                        );
+                    }
+                },
+                bluetooth.disconnect
+            );
+        }catch (e) {
+            Factory.alert("LIGAR BLUETOOTH");
         }
     },
     sendData: function (value) {
